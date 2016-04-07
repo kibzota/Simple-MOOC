57468ae8b3a225916251bf406bd987edabc8f911
kibzota <danilo3457@gmail.com>
2016-04-06 21:52:48 -0400

    frist commit

---
+++ courses/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from courses import views
+
+
+urlpatterns = [
+    url(r'^$', views.index, name="index"),
+    url(r'^(?P<slug>[\w_-]+)$', views.details, name="details")
+]

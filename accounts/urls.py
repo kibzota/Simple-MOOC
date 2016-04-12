4b1a4dd65733928da5673a674045b077c46454ba
kibzota <danilo3457@gmail.com>
2016-04-12 17:44:18 -0400

    Adicionado sistema de login e cadastro de user

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import url
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login, logout
 from accounts import views
 
 urlpatterns = [
     url(r'^entrar/$', login, {
         'template_name': 'accounts/login.html'
     }, name='login'),
+    url(r'^sair/$', logout, {
+        'next_page': 'core:home'
+    }, name='logout'),
 
     url(r'^registrar/$', views.register, name='register')
 ]
--- courses/models.py
+++ courses/models.py
@@ -5,25 +5,26 @@ class CourseManager(models.Manager):
 
     def search(self, query):
         return self.get_queryset().filter(
-            models.Q(name__icontains=query) |
+            models.Q(name__icontains=query) | \
             models.Q(description__icontains=query)
         )
 
 
-class Course (models.Model):
+class Course(models.Model):
 
     name = models.CharField('Nome', max_length=100)
     slug = models.SlugField('Atalho')
     description = models.TextField('Descrição Simples', blank=True)
     about = models.TextField('Sobre o Curso', blank=True)
-    start_date = models.DateField('Data de Início', null=True, blank=True)
+    start_date = models.DateField(
+        'Data de Início', null=True, blank=True
+    )
     image = models.ImageField(
-        upload_to='images', verbose_name=u'Imagem',
+        upload_to='courses/images', verbose_name='Imagem',
         null=True, blank=True
     )
-    created_at = models.DateTimeField(
-        'Criado em', auto_now_add=True
-    )
+
+    created_at = models.DateTimeField('Criado em', auto_now_add=True)
     updated_at = models.DateTimeField('Atualizado em', auto_now=True)
 
     objects = CourseManager()
@@ -31,11 +32,11 @@ class Course (models.Model):
     def __str__(self):
         return self.name
 
+    @models.permalink
     def get_absolute_url(self):
-        from django.core.urlresolvers import reverse
-        return reverse('courses.views.details', args=[str(self.slug)])
+        return ('courses:details', (), {'slug': self.slug})
 
     class Meta:
-        verbose_name = "Curso"
-        verbose_name_plural = "Cursos"
-        ordering = ["name"]
+        verbose_name = 'Curso'
+        verbose_name_plural = 'Cursos'
+        ordering = ['name']

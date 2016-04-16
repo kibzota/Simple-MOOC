a1750591e73f45ee770f47c479ba57618118f76b
kibzota <danilo3457@gmail.com>
2016-04-16 12:26:16 -0400

    adicionado ediçao de perfil

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth.views import login, logout
 from accounts import views
 
 urlpatterns = [
+    url(r'^$', views.dashboard, name='dashboard'),
     url(r'^entrar/$', login, {
         'template_name': 'accounts/login.html'
     }, name='login'),
@@ -10,5 +11,6 @@ urlpatterns = [
         'next_page': 'core:home'
     }, name='logout'),
 
-    url(r'^registrar/$', views.register, name='register')
+    url(r'^registrar/$', views.register, name='register'),
+    url(r'^editar/$', views.edit, name='edit'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-from django.conf import settings
-from .forms import RegisterForm
+from django.contrib.auth import authenticate
+from django.contrib.auth.decorators import login_required
+# from django.conf import settings
+from .forms import RegisterForm, EditAccountForm
+
+
+@login_required
+def dashboard(request):
+    template_name = 'accounts/dashboard.html'
+    return render(request, template_name)
 
 
 def register(request):
@@ -19,4 +26,20 @@ def register(request):
     context = {
         'form': form
     }
+    return render(request, template_name, context)
+
+
+@login_required
+def edit(request):
+    template_name = 'accounts/edit.html'
+    context = {}
+    if request.method == 'POST':
+        form = EditAccountForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            form = EditAccountForm(instance=request.user)
+            context['success'] = True
+    else:
+        form = EditAccountForm(instance=request.user)
+    context['form'] = form
     return render(request, template_name, context)

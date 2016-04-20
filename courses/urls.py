5f3a16b42b507544c2a67de0124217b549f3750c
kibzota <danilo3457@gmail.com>
2016-04-20 16:26:13 -0400

    Melhoria no design do dashboard

--- courses/urls.py
+++ courses/urls.py
@@ -5,6 +5,9 @@ from courses import views
 urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^(?P<slug>[\w_-]+)$', views.details, name="details"),
-    url(r'^(?P<slug>[\w_-]+)/inscricao/$', views.enrollment, name="enrollment")
+    url(r'^(?P<slug>[\w_-]+)/inscricao/$', views.enrollment,
+        name="enrollment"),
+    url(r'^(?P<slug>[\w_-]+)/anuncios/$', views.announcements,
+        name="announcements"),
 
 ]

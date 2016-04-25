68c2365ca8824d1a2cc3bc5772014ca6e1981830
kibzota <danilo3457@gmail.com>
2016-04-24 20:37:56 -0400

    Criado Admin de Aula e Decorator de acesso

--- courses/views.py
+++ courses/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from courses.models import Course, Enrollment
 from .forms import ContactCourse, CommentForm
+from .decorators import enrollment_required
 
 
 def index(request):
@@ -65,15 +66,9 @@ def undo_enrollment(request, slug):
 
 
 @login_required
+@enrollment_required
 def announcements(request, slug):
-    course = get_object_or_404(Course, slug=slug)
-    if not request.user.is_staff:
-        enrollment = get_object_or_404(
-            Enrollment, user=request.user, course=course
-        )
-        if not enrollment.is_approved():
-            messages.error(request, 'A sua inscrição está pendente')
-            return redirect('accounts:dashboard')
+    course = request.course
     template = 'courses/announcements.html'
     context = {
         'course': course,
@@ -83,16 +78,9 @@ def announcements(request, slug):
 
 
 @login_required
+@enrollment_required
 def show_announcement(request, slug, pk):
-    course = get_object_or_404(Course, slug=slug)
-    form = CommentForm(request.POST)
-    if not request.user.is_staff:
-        enrollment = get_object_or_404(
-            Enrollment, user=request.user, course=course
-        )
-        if not enrollment.is_approved():
-            messages.error(request, 'A sua inscrição está pendente')
-            return redirect('accounts:dashboard')
+    course = request.course
     announcement = get_object_or_404(course.announcements.all(), pk=pk)
     form = CommentForm(request.POST or None)
     if form.is_valid():

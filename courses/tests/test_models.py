0e371ab761219a0cffa1a1c3d541d636147796d5
kibzota <danilo3457@gmail.com>
2016-04-27 13:15:37 -0400

    adicionado alguns testes

---
+++ courses/tests/test_models.py
@@ -0,0 +1,30 @@
+from django.core import mail
+from django.test import TestCase
+from django.test.client import Client
+from django.core.urlresolvers import reverse
+from django.conf import settings
+
+from model_mommy import mommy
+
+from courses.models import Course
+
+
+class CourseManagerTestCase(TestCase):
+
+    def setUp(self):
+        self.courses_django = mommy.make(
+            'courses.Course', name='python na Web com Django', _quantity=10)
+        self.courses_dev = mommy.make(
+            'courses.Course', name='python para dev', _quantity=10)
+        self.client = Client()
+
+    def tearDown(self):
+        Course.objects.all().delete()
+
+    def test_course_search(self):
+        search = Course.objects.search('django')
+        self.assertEqual(len(search), 10)
+        search = Course.objects.search('dev')
+        self.assertEqual(len(search), 10)
+        search = Course.objects.search('python')
+        self.assertEqual(len(search), 20)

d8ee62f83d54b54c69d1cd9eb9daa979b0b0f739
kibzota <danilo3457@gmail.com>
2016-04-27 18:26:56 -0400

    heroku deploy

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'simplemooc', 'media')
 MEDIA_URL = '/media/'
@@ -161,6 +161,8 @@ ALLOWED_HOSTS = ['*']
 # Static
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 

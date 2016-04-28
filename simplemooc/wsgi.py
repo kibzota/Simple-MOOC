3956d2cce52e7f3ee5e706b57159323cfae6855e
kibzota <danilo3457@gmail.com>
2016-04-27 23:22:35 -0400

    talvez agora vai

--- simplemooc/wsgi.py
+++ simplemooc/wsgi.py
@@ -8,12 +8,12 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simplemooc.settings")
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+from dj_static import Cling
 
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = Cling(get_wsgi_application())
 
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simplemooc.settings")
 
